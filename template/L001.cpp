ced0ab84c975c21c03b25a01db62568d2c120123
AshenskyABCDE <251902756@qq.com>
2024-04-28 23:18:46 +0800

    2024/4/28

---
+++ template/L001.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+// 简单的模板
+template<typename T>
+T max(T a, T b) {
+    return a > b ? a : b;
+}
+
+//怎么根据变量类型具体实现相关内容？
+template<typename T>
+void func(int x) {
+    std::cout << " 123 " << std::endl;
+}
+
+template<typename T>
+void func(T x) {
+    std::cout << " 123465 " <<std::endl;
+}
+
+template<typename T>
+void func(std::string x) {
+    std::cout << " 123465111 " <<std::endl;
+}
+
+//如果想合到一起呢？用is_same decltype，这叫做类型萃取 判断变量类型是否相同
+template<typename T>
+void func1(T x) {
+    int y;
+    if ( std::is_same<decltype(x) , decltype(y)>::value ) { // 等同于 std::is_same<decltype(x) , int>::value
+        std::cout << "int" << std::endl;
+    } else {
+        std::cout<< "dont int a" << std::endl;
+    }
+}
+int main() {
+    func<int>(1); // 123
+    func<double>(1.0); // 123465
+    func<std::string>("123");// 123465111
+
+    std::cout << "---------" << std::endl;
+    func1<int>(1);
+    func1<std::string>("123");
+}
---
+++ template/readme.md
@@ -0,0 +1,8 @@
+C++模板的个人学习过程，如果有疑问欢迎提交，讨论学习可以来联系我
+
+学习参考资料：
+推荐mq白的模板课程https://www.bilibili.com/cheese/play/ep468088?csource=private_space_tougao_null&spm_id_from=333.999.0.0
+
+2024/4/28
+对应L001，更新了简单的模板，以及一些相关的介绍，此阶段可以自行实现一些Max，Min等简单的模板
+
