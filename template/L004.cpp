22f0348ce42d3c206c7f2722e330bbfbb7214792
AshenskyABCDE <251902756@qq.com>
2024-04-30 15:44:02 +0800

    2024/4/30

--- template/L004.cpp
+++ template/L004.cpp
@@ -13,7 +13,7 @@ void func1 (int* x, double* xx, const char** xxx) {
 }
 template<typename... Args> //Args是类型型参包，变量名可以自己改，里面存放的是对应的变量类型
 void sum(Args... args) { // args是函数型参包，变量名也可以自己改，里面存放的是对应的参数
-    func1(args...);
+    func1(args...);// ....
     func1(&args...);
     // 你可以发现...就是展开的意思，专有名词就叫包展开
 }
