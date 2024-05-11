e0610984f84685e77cbb8263e96fb6bf8fcf69ac
AshenskyABCDE <251902756@qq.com>
2024-05-11 20:46:59 +0800

    2024/5/11

---
+++ test/szof.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <utility>
+
+int main() {
+    char *s = "abcdefeafaefe";
+    std::cout << sizeof(s) << std::endl;
+    char ss[] = {'a', 'b', 'c', 'd' , 'e'};
+    std::cout << sizeof(ss) << std::endl;
+    int a[] = {1, 2, 4 ,5,6};
+    std::cout << sizeof(a) << std::endl;  
+    string sss = "123456";
+    std::cout << sizeof(sss) << std::endl;   
+}
