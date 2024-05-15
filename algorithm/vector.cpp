1198ab3d53afc5ae51bbf96652ca3d1589a5d1c4
AshenskyABCDE <251902756@qq.com>
2024-05-15 20:08:09 +0800

    2024/5/15

--- algorithm/vector.cpp
+++ algorithm/vector.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-template<typename T>
+template <typename T>
 class Vector {
 private:
-    T* data;
-    int capacity;
     int size;
-    void resize(int new_capacity) {
+    int capacity;
+    T* data ;
+    void resize( int new_capacity ) {
         T* new_data = new T[new_capacity];
-        for(int i=0;i< size;i++){
+        for(int i=0;i < size;i++) {
             new_data[i] = data[i];
         }
         delete[] data;
@@ -16,13 +16,14 @@ private:
         capacity = new_capacity;
     }
 public:
-    Vector() : data(nullptr) , capacity(0) , size(0){}
+    Vector() : data(nullptr), size(0) , capacity(0) {}
     ~Vector() {
         delete[] data;
     }
+
     void push_back(const T& value) {
         if(size == capacity) {
-            if (size == 0) {
+            if(size == 0) {
                 resize(1);
             } else {
                 resize(capacity * 2);
@@ -30,17 +31,32 @@ public:
         }
         data[size++] = value;
     }
-    T& operator[](int index) {
-        if(index <= size)
+
+    void erase(int pos) {
+        if( pos >= size || pos < 0) return ;
+        for(int i=pos-1;i<size-1;i++){
+            data[i] = data[i+1];
+        }
+        size -- ;
+    }
+
+    T& operator[] (int index) {
+        if (index < size)
         return data[index];
     }
 };
+
 int main() {
-    Vector<int> vec;
-    for(int i=1;i<=10;i++) {
+    Vector<int>vec;
+    for(int i=0;i<10;i++) {
         vec.push_back(i);
     }
-    for(int i=0;i<=9;i++){
+    for(int i=0;i<10;i++) {
         std::cout << vec[i] << std::endl;
     }
+    std::cout << "---------------------erase------------" << std::endl;
+    vec.erase(5);
+    for(int i=0;i<10;i++) {
+        std::cout << vec[i] << std::endl;
+    } 
 }
